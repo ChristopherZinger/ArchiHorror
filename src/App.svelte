10c862c50e0dc41472260deb65303833d8c8bde0
ChristopherZinger <christopher@van-ons.nl>
2021-10-23 13:39:10 +0200

    [AU-01] display user name in header and manage login /logout button

--- src/App.svelte
+++ src/App.svelte
@@ -2,16 +2,7 @@
   import { Router, Route } from 'svelte-navigator';
   import Home from './pages/Home.svelte';
   import Auth from './pages/Auth.svelte';
-  import { userStore } from './stores/auth';
-
-  let myuser; 
-
-  userStore.subscribe(_user => myuser = _user);
-
-  console.log(myuser);
-
 </script>
-
 
 <Router>
   <Route path='/'>
@@ -28,6 +19,3 @@
     </div>
   </Route>
 </Router>
-
-  <p>User name is : {myuser?.email || '--is not logged--'}</p>
-
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,14 +1,27 @@
 <script lang="ts">
   import HeaderItem from '../atoms/HeaderItem.svelte';
+import SignoutButton from '../atoms/SignoutButton.svelte';
+  import { userStore } from '../stores/auth';
 
+  let user; 
+  userStore.subscribe(userFromStore => user = userFromStore);
 
 </script>
 
 <header>
   <div class="container">
     <HeaderItem link={{text: 'Home', url: '//'}} />
-    <HeaderItem link={{text: 'Login', url: '//login'}} />
+    {#if user}
+      <p>{ user.email}</p>
+    {/if}
+
+    {#if user}
+      <SignoutButton ></SignoutButton>
+    {:else}
+      <HeaderItem link={{text: 'Login', url: '//login'}} />
+    {/if}
   </div>
+
 </header>
 
 
