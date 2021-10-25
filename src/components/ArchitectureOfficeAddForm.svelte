a8b66bac189b0f7a729086d99303cd46172f7542
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-10-25 14:28:59 +0200

    [DM-03] architecture office create form component

---
+++ src/components/ArchitectureOfficeAddForm.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { architectureOffice } from '../services/domains/architectureOffice';
+
+  let office = {
+    name: '',
+    city: ''
+  }
+
+  const submit = () => {
+    architectureOffice.addDocument(office);
+  }
+</script>
+
+<div>
+  <h4>Add Office</h4>
+  <form on:submit|preventDefault={submit}>
+    <label for="office-name">Office Name:</label>
+    <input type="text" id="office-name" bind:value={office.name} />
+    
+    <label for="office-city">Office City:</label>
+    <input type="text" id="office-name" bind:value={office.city} />
+
+    <button type="submit">Submit new office : {office.name}</button>
+  </form>
+</div>
