53a37e6c18d8cef8180ae6f3a323e4b6472dac65
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-10-28 14:23:22 +0200

    [DM-03-2] add creator and slug field when creating new office

--- src/services/domains/architectureOffice.ts
+++ src/services/domains/architectureOffice.ts
@@ -1,10 +1,11 @@
 import { db } from '../firebase/connectToFirebaseDB';
-import { collection, getDocs, addDoc, Firestore } from 'firebase/firestore';
+import { collection, addDoc, doc, Firestore, getDoc, setDoc } from 'firebase/firestore';
 import { BaseRecord } from './baseRecord';
+import { getAuth } from 'firebase/auth';
+import slugify from 'slugify';
 
 export interface ArchitectureOfficeRecord {
   name: string;
-  createdAt: Date | undefined
   city: string
 }
 
@@ -13,15 +14,50 @@ class ArchitectureOffice extends BaseRecord<ArchitectureOfficeRecord> {
     super(db, 'architecture-offices');
   }
 
-  public async addDocument (document: ArchitectureOfficeRecord) {
+  public async create (document: ArchitectureOfficeRecord) {
+    const doc = this.populateDocumentWithRequiredFields(document);
+
+    if (!doc) {
+      throw new Error('Could not create new office.');
+    }
+
     try {
-      const createdDoc = await addDoc(collection( this.db, this.collectionName ), document);
-      console.log("Created Doc: ", createdDoc);
-      return createdDoc;
+      return await addDoc(collection( this.db, this.collectionName ), doc);
     } catch (err) {
       console.error(err);
       return err;
     }
+  }
+
+  private populateDocumentWithRequiredFields (office) {
+    try {
+      return {
+        ...document,
+        ...this.addCreatorField(),
+        ...this.addSlugField(office),
+      }
+    } catch (err) {
+      console.warn(err.message);
+      return;
+    }
+  }
+
+  private addCreatorField () {
+    const userID  = getAuth()?.currentUser?.uid;
+
+    if (!userID) {
+      throw new Error('You have to be logged in to add an office.');
+    }
+
+    return { createdBy: userID}
+  }
+
+  private addSlugField (office) {
+    const slug = slugify(office.name + '--' + office.city, {
+      lower: true,
+      strict: true,
+    })
+    return { slug }
   }
 }
 
