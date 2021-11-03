32d12aff6135d09e07897dd82f72bb15d662b683
ChristopherZinger <krzysztof.zinger@gmail.com>
2021-11-03 16:34:37 +0100

    [FU-01] connect to emulators

--- src/services/firebase/createFirebaseApp.ts
+++ src/services/firebase/createFirebaseApp.ts
@@ -1,14 +1,27 @@
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 import { initializeApp } from 'firebase/app';
-import { getAuth, Auth }  from 'firebase/auth';
+import { getAuth, Auth, connectAuthEmulator }  from 'firebase/auth';
+import { getFunctions, connectFunctionsEmulator } from "firebase/functions";
 import config from './config';
 
 class FirebaseApp {
   auth: Auth;
   app;
+  db;
 
   constructor () {
     this.app = initializeApp(config);
     this.auth = getAuth();
+    this.db = getFirestore();
+    this.connectToEmulators();
+  }
+
+  private connectToEmulators () {
+    connectFirestoreEmulator(this.db, 'localhost', 8080);
+    connectAuthEmulator(this.auth, "http://localhost:9099");
+
+    const functions = getFunctions(this.app);
+    connectFunctionsEmulator(functions, "localhost", 5001);
   }
 }
 
